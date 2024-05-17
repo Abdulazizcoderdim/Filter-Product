6118afdd5798ef63e8ff8bdfe2aefc7a341cc4f7
AbdulazizRus <coderdim@gmail.com>
2024-05-17 21:28:31 +0500

    changes

--- src/components/Products/ProductSkeleton.tsx
+++ src/components/Products/ProductSkeleton.tsx
@@ -1,13 +1,13 @@
-
 const ProductSkeleton = () => {
   return (
     <div>
-        
-    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
-        
-    </div>
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+         
+      </div>
     </div>
   )
 }
 
 export default ProductSkeleton
+
+//1:05:00
