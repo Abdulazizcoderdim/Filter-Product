c82e197be692ce06903056a31aa158e90eb03f27
AbdulazizRus <coderdim@gmail.com>
2024-05-18 10:33:34 +0500

    size filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,13 @@ import { ChevronDown, Filter } from 'lucide-react'
 import { useState } from 'react'
 import type { Product as TProduct } from '@/db'
 import ProductSkeleton from '@/components/Products/ProductSkeleton'
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+import { ProdcutState } from '@/lib/validators/product-validator'
 
 const SORT_OPTIONS = [
   { name: 'None', value: 'none' },
@@ -23,15 +29,25 @@ const SORT_OPTIONS = [
 ] as const
 
 const COLOR_FILTERS = {
+  id: 'size',
+  name: 'Size',
+  options: [
+    { value: 'white', label: 'White' },
+    { value: 'beige', label: 'Beige' },
+    { value: 'blue', label: 'Blue' },
+    { value: 'green', label: 'Green' },
+    { value: 'purple', label: 'Purple' },
+  ],
+} as const
+
+const SIZE_FILTERS = {
   id: 'color',
   name: 'Color',
   options: [
-    {value: 'white', label: 'White'},
-    {value: 'beige', label: 'Beige'},
-    {value: 'blue', label: 'Blue'},
-    {value: 'green', label: 'Green'},
-    {value: 'purple', label: 'Purple'},
-  ] as const
+    { value: 'S', label: 'S' },
+    { value: 'M', label: 'M' },
+    { value: 'L', label: 'L' },
+  ] as const,
 }
 
 const SUBCATEGORIES = [
@@ -41,10 +57,17 @@ const SUBCATEGORIES = [
   { name: 'Accessories', selected: false, href: '#' },
 ]
 
+const DEFAULT_CUSTOM_PRICE = [0, 100] as [number, number]
+
 export default function Home() {
-  const [filter, setFilter] = useState({
+  const [filter, setFilter] = useState<ProdcutState>({
+    color: ['beige', 'blue', 'green', 'purple', 'white'],
+    price: { isCustom: false, range: DEFAULT_CUSTOM_PRICE },
+    size: ['L', 'M', 'S'],
     sort: 'none',
   })
+
+  console.log(filter)
 
   const { data: products } = useQuery({
     queryKey: ['products'],
@@ -62,7 +85,27 @@ export default function Home() {
     },
   })
 
-  console.log(products)
+  const applyArrayFilter = ({
+    category,
+    value,
+  }: {
+    category: keyof Omit<typeof filter, 'price' | 'sort'>
+    value: string
+  }) => {
+    const isFilterApplied = filter[category].includes(value as never)
+
+    if (isFilterApplied) {
+      setFilter((prev) => ({
+        ...prev,
+        [category]: prev[category].filter((v) => v !== value),
+      }))
+    } else {
+      setFilter((prev) => ({
+        ...prev,
+        [category]: [...prev[category], value],
+      }))
+    }
+  }
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -112,34 +155,84 @@ export default function Home() {
             <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm">
               {SUBCATEGORIES.map((category) => (
                 <li key={category.name}>
-                  <button disabled={!category.selected} className="disabled:cursor-not-allowed disabled:opacity-60">
+                  <button
+                    disabled={!category.selected}
+                    className="disabled:cursor-not-allowed disabled:opacity-60"
+                  >
                     {category.name}
                   </button>
                 </li>
               ))}
             </ul>
 
-            <Accordion type='multiple' className='animate-none'>
-               {/* Color filter */}
-               <AccordionItem value='color'>
-                 <AccordionTrigger className='py-3 text-sm text-gray-400 hover:text-gray-500'>
-                    <span className='font-medium text-gray-900'>Color</span>
-                 </AccordionTrigger>
+            <Accordion type="multiple" className="animate-none">
+              {/* Color filter */}
+              <AccordionItem value="color">
+                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900">Color</span>
+                </AccordionTrigger>
 
-                 <AccordionContent className='pt-6 animate-none'>
-                    <ul className='space-y-4'>
-                      {COLOR_FILTERS.options.map((option,optionIdx)=>(
-                        <li className='flex items-center' key={option.value}>
-                           <input className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500' type="checkbox" id={`color=${optionIdx}`} />
-                           <label className='ml-3 text-sm text-gray-600' htmlFor={`color=${optionIdx}`}>
-                            {option.label}
-                           </label>
-                        </li> 
-                      ))}
-                    </ul>              
-                 </AccordionContent>
+                <AccordionContent className="pt-6 animate-none">
+                  <ul className="space-y-4">
+                    {COLOR_FILTERS.options.map((option, optionIdx) => (
+                      <li className="flex items-center" key={option.value}>
+                        <input
+                          onChange={() => {
+                            applyArrayFilter({
+                              category: 'color',
+                              value: option.value,
+                            })
+                          }}
+                          checked={filter.color.includes(option.value)}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                          type="checkbox"
+                          id={`color-${optionIdx}`}
+                        />
+                        <label
+                          className="ml-3 text-sm text-gray-600"
+                          htmlFor={`color-${optionIdx}`}
+                        >
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
 
-               </AccordionItem>
+              {/* Size filter */}
+              <AccordionItem value="size">
+                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900">Size</span>
+                </AccordionTrigger>
+
+                <AccordionContent className="pt-6 animate-none">
+                  <ul className="space-y-4">
+                    {SIZE_FILTERS.options.map((option, optionIdx) => (
+                      <li className="flex items-center" key={option.value}>
+                        <input
+                          onChange={() => {
+                            applyArrayFilter({
+                              category: 'size',
+                              value: option.value,
+                            })
+                          }}
+                          checked={filter.size.includes(option.value)}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                          type="checkbox"
+                          id={`size-${optionIdx}`}
+                        />
+                        <label
+                          className="ml-3 text-sm text-gray-600"
+                          htmlFor={`size-${optionIdx}`}
+                        >
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
             </Accordion>
           </div>
 
@@ -162,4 +255,4 @@ export default function Home() {
   )
 }
 
-// 1:16:00
+// 1:48:00
